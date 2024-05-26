8d7dcf9b9ef676a6df1b4e417dc3bedab1a8c0c4
Rafael <rafael.augustoklein04@gmail.com>
2024-05-25 23:52:57 -0300

    feat:iniciando estrutura do player com side bar e albuns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,131 @@
-import Image from "next/image";
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play} from 'lucide-react'
+import Image from 'next/image';
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-72 bg-zinc-950 p-6">
+          <div className='flex items-center gap-2'>
+            <div className='w-3 h-3 bg-red-500 rounded-full'></div>
+            <div className='w-3 h-3 bg-yellow-500 rounded-full'></div>
+            <div className='w-3 h-3 bg-green-500 rounded-full'></div>
+          </div>
+
+          <nav className='space-y-5 mt-8'>
+            <a href="" className="flex itens-center gap-3 text-sm font-semibold text-zinc-300">
+              <HomeIcon />
+              Home
+            </a>
+            <a href="" className="flex itens-center gap-3 text-sm font-semibold text-zinc-300">
+              <Search />
+              Search
+            </a>
+            <a href="" className="flex itens-center gap-3 text-sm font-semibold text-zinc-300">
+              <Library />
+              Your Library
+            </a>
+          </nav>
+
+          <nav className='mt-7 pt-7 border-t border-zinc-800 flex flex-col gap-3'>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Hot Hits Brasil</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Funk 2024</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Aniversário</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Sertanejo Antigas</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Coding and Music</a>
+          </nav>
+        </aside>
+        <main className="flex-1 p-6">
+          <div className='flex items-center gap-4'>
+            <button className='rounded-full bg-black/20 p-1'>
+              <ChevronLeft />
+            </button>
+            <button className='rounded-full bg-black/20 p-1'>
+              <ChevronRight />
+            </button>
+          </div>
+          <h1 className='font-semibold text-3xl mt-10'>Good Afternoom</h1>
+
+          <div className='grid grid-cols-3 gap-6 mt-5'>
+            <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/Dark_Side_of_the_Moon.png" width={104} height={104} alt='Capa Pink Floyd'/>
+              <strong>Money</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+            <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/Dark_Side_of_the_Moon.png" width={104} height={104} alt='Capa Pink Floyd'/>
+              <strong>Money</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+            <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/Dark_Side_of_the_Moon.png" width={104} height={104} alt='Capa Pink Floyd'/>
+              <strong>Money</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+            <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/Dark_Side_of_the_Moon.png" width={104} height={104} alt='Capa Pink Floyd'/>
+              <strong>Money</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+            <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/Dark_Side_of_the_Moon.png" width={104} height={104} alt='Capa Pink Floyd'/>
+              <strong>Money</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+            <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src="/Dark_Side_of_the_Moon.png" width={104} height={104} alt='Capa Pink Floyd'/>
+              <strong>Money</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play fill='bg-black'/>
+              </button>
+            </a>
+          </div>
+
+          <h2 className='font-semibold text-2xl mt-10'>Made for Rafael Klein</h2>
+
+          <div className='grid grid-cols-7 gap-4 mt-4'>
+            <a href='' className='bg-white/5 p-3 rounded flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/Dark_Side_of_the_Moon.png" className='w-full' width={120} height={120} alt='Capa Pink Floyd'/>
+              <strong className='font-semibold'>Daily Mix 1</strong>
+              <span className='text-sm text-zinc-400'>Hugo e Guilerme, Henrique e Juliano and more</span>
+            </a>
+            <a href='' className='bg-white/5 p-3 rounded flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/Dark_Side_of_the_Moon.png" className='w-full' width={120} height={120} alt='Capa Pink Floyd'/>
+              <strong className='font-semibold'>Daily Mix 1</strong>
+              <span className='text-sm text-zinc-400'>Hugo e Guilerme, Henrique e Juliano and more</span>
+            </a>
+            <a href='' className='bg-white/5 p-3 rounded flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/Dark_Side_of_the_Moon.png" className='w-full' width={120} height={120} alt='Capa Pink Floyd'/>
+              <strong className='font-semibold'>Daily Mix 1</strong>
+              <span className='text-sm text-zinc-400'>Hugo e Guilerme, Henrique e Juliano and more</span>
+            </a>
+            <a href='' className='bg-white/5 p-3 rounded flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/Dark_Side_of_the_Moon.png" className='w-full' width={120} height={120} alt='Capa Pink Floyd'/>
+              <strong className='font-semibold'>Daily Mix 1</strong>
+              <span className='text-sm text-zinc-400'>Hugo e Guilerme, Henrique e Juliano and more</span>
+            </a>
+            <a href='' className='bg-white/5 p-3 rounded flex flex-col gap-2 hover:bg-white/10'>
+              <Image src="/Dark_Side_of_the_Moon.png" className='w-full' width={120} height={120} alt='Capa Pink Floyd'/>
+              <strong className='font-semibold'>Daily Mix 1</strong>
+              <span className='text-sm text-zinc-400'>Hugo e Guilerme, Henrique e Juliano and more</span>
+            </a>
+          </div>
+        </main>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+        footer
+      </footer>
+    </div>
   );
 }
